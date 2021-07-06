0d17dc9b067623e0c8e2d40fe958bfcb6e650ec0
vagman <bmanousakis@gmail.com>
2021-07-06 17:09:44 +0300

    refactored code to be Python 3 compatible

--- Days.py
+++ Days.py
@@ -1,4 +1,4 @@
-#Days Askisi 7
+# Days Exercise 7
 
 import datetime
 from datetime import date
@@ -7,9 +7,9 @@ sum_day = 0
 days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
 
 today = datetime.datetime.today()
-today_str = int(today.strftime("%d")) # 10
-today_name = today.strftime("%A") # Saturday
-today_year = int(today.strftime("%Y")) # 2018
+today_str = int(today.strftime("%d")) # ex. 10
+today_name = today.strftime("%A") # ex. Saturday
+today_year = int(today.strftime("%Y")) # ex. 2018
 
 for y in range(today_year + 1,today_year + 11):
 	for m in range(1,13):
@@ -18,10 +18,12 @@ for y in range(today_year + 1,today_year + 11):
 		else:
 			days_of_month = date(y, m + 1, 1) - date(y, m, 1)
 		days_of_month = str(days_of_month)[0:2]
+		
 		for d in range(1,int(days_of_month) + 1):
 			myDate = datetime.date(y,m,d)
 			day = days[myDate.weekday()]
 			if d == today_str: 
 				if today_name == day:
 					sum_day += 1
-print "There will be ", sum_day , today_name + "s" , "which will be the ", str(today_str) + "th", "of the month , in the next 10 years."
+
+print("There will be {} {}s which will be the {}th, of the month, in the next 10 years.".format(sum_day , today_name, today_str))
--- Kino.py
+++ Kino.py
@@ -1,11 +1,12 @@
-#Kino Askisi 2
+# Kino Exercise 2
 
-import urllib2
+from urllib import request 
+from urllib.request import urlopen
 import json
 import datetime
 
 numb_list = []
-numb = raw_input("Give me 10 numbers seperated by commas: ")
+numb = input("Give me 10 numbers seperated by commas: ")
 numbs = numb.split(",")
 for i in range(10):
 	numb_list.append(int(numbs[i]))
@@ -15,20 +16,20 @@ days = []
 cur_date = datetime.datetime.today()
 
 for i in range(31):
-	cur_date= cur_date - datetime.timedelta(days=1)
-	date_str= cur_date.strftime("%d-%m-%Y")     
-	url='http://applications.opap.gr/DrawsRestServices/kino/drawDate/%s.json'%date_str 
-	req = urllib2.Request(url)          
-	response = urllib2.urlopen(req)     
+	cur_date = cur_date - datetime.timedelta(days=1)
+	date_str = cur_date.strftime("%d-%m-%Y")     
+	url = "http://applications.opap.gr/DrawsRestServices/kino/drawDate/{}.json".format(date_str) 
+	req = request(url)          
+	response = urlopen(req)     
 	data = response.read()              
-	data=json.loads(data)               
-	klhrwseis= data['draws']['draw']      
+	data = json.loads(data)               
+	lottery = data["draws"]["draw"]      
 	
 	days.append(date_str)
 	r = []
 	success_sum = 0 # Counts the day's success rate
 
-	for k in klhrwseis:
+	for k in lottery:
 		sum_4 = 0  # This is the sum that counts the 4 or more numbers
 		lottery = k["results"]
 		r.append(lottery)
@@ -41,7 +42,7 @@ for i in range(31):
 	success.append(success_sum)
 	
 # Here we sort 2 parallel lists so we can print the day with the most success rate !
-for length in range(len(success) - 1,0,-1):
+for length in range(len(success) - 1, 0, -1):
 	for i in range(length):
 		if success[i] > success[i + 1]:
 			temp1 = success[i]
@@ -52,4 +53,5 @@ for length in range(len(success) - 1,0,-1):
 			days[i] = days[i + 1]
 			days[i + 1] = temp2
 x = success.index(max(success))
-print "With the numbers you chose, your luckiest day is: ", days[x]
+
+print("With the numbers you chose, your luckiest day is: ", days[x])
