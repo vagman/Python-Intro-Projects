bb5955c6db301d67cb0111196a4c3554489e24ae
Vaggelis Manousakis <bmanousakis@gmail.com>
2021-09-23 00:01:39 +0300

    deleted files

--- 2021 Assignments/twitter/main.py
+++ 2021 Assignments/twitter/main.py
@@ -10,10 +10,10 @@ auth.set_access_token(con.access_token, con.access_token_secret)
 api = tweepy.API(auth)
 
 # TODO: Enter user's username to scrape his tweets..
-username = input("Give me a username to scrape the last 10 tweets :")
+username = input("\nAvailable users for scraping:\n1)Bloomberg\n2)CNBC\n3)NASA\nGive me a username to scrape the last 10 tweets from: ")
 
 users_tweets = api.user_timeline(username, count = 10)
 for count, tweet in enumerate(users_tweets):
     print(count, tweet.text)
-    
+    tweet_
 
