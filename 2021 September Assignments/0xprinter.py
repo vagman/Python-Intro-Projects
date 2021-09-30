010ffca926fed51919c1f4bacfd419d7f20896bc
vagman <bmanousakis@gmail.com>
2021-09-30 09:08:45 +0300

    improved files/folders

--- 2021 September Assignments/0xprinter.py
+++ 2021 September Assignments/0xprinter.py
@@ -6,31 +6,32 @@ try:
     f = open(filename, "r")
     data = f.read()
     
-    result_string = ""
+    result = ""
     for character in data:
         ascii_value = ord(character)
 
         # ASCII Functional character eg. space, tab, sound
         if ascii_value <= 32:
-            result_string += " "
+            result += " "
 
         # A-Za-z in ASCII
         elif ascii_value >= 65 and ascii_value <= 90 or ascii_value >= 97 and ascii_value <= 122:
-            result_string += "A"
+            result += "A"
 
         # 0-9 digits
         elif re.match("[0-9]", character):     
-            result_string += "1"
+            result += "1"
         
         # ASCII values < 128
         elif ascii_value < 128:
-            result_string += "p"
-            
+            result += "p"
+
+        # nonASCII
         else:
-            result_string += "?"
+            result += "?"
 
     # Print 16 characters/line
-    print("\n".join(wrap(text = result_string, width = 16)))
+    print("\n".join(wrap(text = result, width = 16)))
 
     f.close()
 except IOError:
